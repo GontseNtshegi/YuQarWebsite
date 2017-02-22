cb8e045c115fc30910d97866e2ed7990dc791628
Ntshegi <ntshegg@lhc.local>
2017-02-22 10:22:03 +0200

    Yeoman scaffolding init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yuQarWebsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yuQarWebsiteApp
+ */
+angular.module('yuQarWebsiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yuQarWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yuQarWebsiteApp
+ */
+angular.module('yuQarWebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
